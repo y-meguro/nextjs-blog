09799dccacda6642a08fcf682df5bab49e33df63
Yohei Meguro <y.meguro.2008@gmail.com>
2021-03-09 15:23:55 +0900

    update name

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Yohei Meguro'
+const name = 'Test Taro'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
